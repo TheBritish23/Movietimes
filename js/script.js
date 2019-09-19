603998efb3e6e9fda1c83ee063763c3b9b0cd2d9
Soro Cat <sorousha326@gmail.com>
2019-09-19 10:33:01 -0700

    add changes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 //business logic for Audience
-
+function Audience() {
+  this.Movies = [],
+  this.currentId = 0
+}
 
 
 //business logic for Movies
 
+// User Interface Logic
 $(document).ready(function() {
+  $("form#showtime").submit(function(event) {
+    event.preventDefault()
