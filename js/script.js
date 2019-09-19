5a74a2d18c7860aecf383612e5fbad2bd8633ef1
Soro Cat <sorousha326@gmail.com>
2019-09-19 10:49:09 -0700

    add changes

--- js/script.js
+++ js/script.js
@@ -8,6 +8,17 @@ function Audience() {
 //business logic for Movies
 
 // User Interface Logic
+function showAudience(contactId) {
+  var movie = addressBook.findContact(contactId);
+  $("#show-contact").show();
+  $(".number-people").html(movie.firstName);
+  $(".cinema").html(movie.lastName);
+  $(".money-change").html(movie.phoneNumber);
+}
+
+
+
+
 $(document).ready(function() {
   $("form#showtime").submit(function(event) {
     event.preventDefault()
