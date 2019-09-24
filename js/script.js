5c66194cb1ce257b25b8e81ff28ecaf574519dc9
Soro Cat <sorousha326@gmail.com>
2019-09-24 12:39:04 -0700

    add changes

--- js/script.js
+++ js/script.js
@@ -4,17 +4,17 @@ function Audience() {
   this.currentId = 0
 }
 
-Audience.prototype.addTickets = function(Tickets) {
-  contact.id = this.assignId();
-  this.tickets.push(contact);
-}
+//Audience.prototype.addTickets = function(Tickets) {
+  //tickets.id = this.assignId();
+  //this.tickets.push(tickets);
+//}
 
-AddressBook.prototype.assignId = function() {
+Audience.prototype.assignId = function() {
   this.currentId += 1;
   return this.currentId;
 }
 
-AddressBook.prototype.findTickets = function(id) {
+Audience.prototype.findTickets = function(id) {
   for (var x=0; x < this.Tickets.length; x++) {
     if (this.Tickets[x]) {
       if (this.Tickets[x].id == id) {
@@ -26,14 +26,32 @@ AddressBook.prototype.findTickets = function(id) {
 }
 
 //business logic for Tickets
-function tickets(changE, moviE, timE) {
+function Tickets(peoplE, moviE, timE) {
   this.peoplE = peoplE;
   this.moviE = moviE;
   this.timE = timE;
 }
 
+Tickets.prototype.fullName = function() {
+  return this.peoplE + " " + this.timE;
+}
+
+Tickets.prototype.pricetag = function() {
+  console.log(this.peoplE);
+  return parseInt(this.peoplE) * 13.35;
+}
+
 // User Interface Logic
 var audience = new Audience();
+
+function displayTicketsDetails(audienceToDisplay) {
+  var ticketsList = $("ul#tickets");
+  var htmlForticketsInfo = "";
+  addressBookToDisplay.tickets.forEach(function(contact) {
+    htmlForTicketsInfo += "<li id=" + tickets.id + ">" + tickets.peoplE + " " + tickets.filM + " " + tickets.timE + "</li>";
+  });
+  ticketsList.html(htmlForTicketsInfo);
+};
 
 function showAudience(audienceId) {
   var movie = auDience.findContact(audienceId);
@@ -44,23 +62,19 @@ function showAudience(audienceId) {
   var buttons = $("#buttons");
 }
 
-function attachMoviesListeners() {
-  $("").on
-
-
-
 $(document).ready(function() {
-  attachContactListeners();
   $("form#showtime").submit(function(event) {
     event.preventDefault()
-    var inputtedNumberPeople = $("input#new-peoplE").val();
-    var inputtedfilm = $("input#new-moviE").val();
-    var inputtedtime = $("input#new-timE").val();
-    $("input#new-peoplE").val("");
-    $("input#new-moviE").val("");
-    $("input#new-timE").val("");
+    var inputtedNumberPeople = $("input#peoplE").val();
+    var inputtedfilm = $("input#filM").val();
+    var inputtedtime = $("input#timE").val();
+    $("input#peoplE").val("");
+    $("input#filM").val("");
+    $("input#timE").val("");
     var newTickets = new Tickets(inputtedNumberPeople, inputtedfilm, inputtedtime);
-    audience.addTickets(newTickets);
-    displayTicketDetails(audience);
+    var ticketPrice = newTickets.pricetag()
+    console.log(ticketPrice);
+    //audience.addTickets(newTickets);
+    //displayTicketDetails(audience);
   })
- });
+ })
